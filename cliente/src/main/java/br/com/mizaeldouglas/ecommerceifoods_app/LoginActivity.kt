b2582e50af222f1b3a138f2dcb83192d762ce03a
MizaelDouglasDeMello <mizaeldouglas4@gmail.com>
2025-01-21 20:27:25 -0300

    feat: created design login

--- cliente/src/main/java/br/com/mizaeldouglas/ecommerceifoods_app/LoginActivity.kt
+++ cliente/src/main/java/br/com/mizaeldouglas/ecommerceifoods_app/LoginActivity.kt
@@ -1,5 +1,6 @@
 package br.com.mizaeldouglas.ecommerceifoods_app
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.mizaeldouglas.ecommerceifoods_app.databinding.ActivityLoginBinding
@@ -9,6 +10,7 @@ class LoginActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -16,6 +18,14 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun initializer() {
+        initializerEventClick()
+    }
 
+    private fun initializerEventClick() {
+        binding.txtCadastro.setOnClickListener {
+            startActivity(
+                Intent(this, CadastroActivity::class.java)
+            )
+        }
     }
 }
